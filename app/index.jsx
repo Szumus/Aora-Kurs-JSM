1bffc10f00920ecf5c663d958c8861af205a0429
szumus <szymonmrozek191@gmail.com>
2025-02-22 22:39:51 +0100

    Folder structur, Tabs bar and icons'

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pblack ">Aora!</Text>
+
+      <Link href="/home"> Home</Link>
+    </View>
+  );
+}
